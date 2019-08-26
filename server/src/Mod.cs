139352bab70865377aaa907668926eada10b6e05
Somfic <somfic@gmail.com>
2019-08-26 20:17:34 +0200

    Rewritten documentation code.

---
+++ server/src/Mod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace kRPC2
+{
+    // Aug 25th 2019
+
+    [KSPAddon(KSPAddon.Startup.AllGameScenes, false)]
+    public class Mod : MonoBehaviour
+    {
+
+    }
+}
